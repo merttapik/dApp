ae130fa8b9fd99ba8ce5f04304539df6a8444c11
merttapik <78278144+merttapik@users.noreply.github.com>
2022-10-27 01:17:27 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+
+import Nav from "../layout/Nav.js";
+
+
+
+
+
+
+
+
+function HomePage() {
+    return (
+        <div >
+        <Nav />
+        <button className="btn btn-secondary" >
+        Launch demo modal
+      </button>
+     
+     
+      
+          
+        </div>
+
+      );
+}
+
+export default HomePage;
